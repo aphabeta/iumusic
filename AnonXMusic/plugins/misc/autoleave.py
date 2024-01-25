b0eede33937c09bfc15efb1a38c4d49b8c12035f
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2024-01-25 13:02:23 +0530

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -28,6 +28,7 @@ async def auto_leave():
                                 i.chat.id != config.LOGGER_ID
                                 and i.chat.id != -1001686672798
                                 and i.chat.id != -1001549206010
+                                and i.chat.id != -1001424478315
                             ):
                                 if left == 20:
                                     continue
